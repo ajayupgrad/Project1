bc49bb34cbf15fdc2a460ab28aa85dddf277dd21
ajayupgrad@gmail.com <gayatreemishra@Gayatrees-MacBook-Pro.local>
2019-12-02 00:33:44 +0530

    Added files related to CommentService and Comment Model and added the relevent controller

--- ImageHoster/src/main/java/ImageHoster/service/CommentService.java
+++ ImageHoster/src/main/java/ImageHoster/service/CommentService.java
@@ -1,6 +1,32 @@
 package ImageHoster.service;
 
+import ImageHoster.model.Comment;
+import ImageHoster.model.Image;
+import ImageHoster.repository.CommentRepository;
+import ImageHoster.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CommentService {
 
 
-}
+        @Autowired
+        private CommentRepository commentRepository;
+
+        //Call the getAllImages() method in the Repository and obtain a List of all the images in the database
+        public List<Comment> getAllComments() {
+        return commentRepository.findAllComments();
+    }
+
+        //The method calls the updateImage() method in the Repository and passes the Image to be updated in the database
+        public void createComment(Comment comment) {
+        commentRepository.createComment(comment);
+    }
+
+    }
+
+
+
